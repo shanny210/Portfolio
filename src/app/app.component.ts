ae30d8e0d622ea28d44fb222af386b64c458072a
Shannon <37170995+shanny210@users.noreply.github.com>
2024-06-01 15:45:04 +0200

    Mit Willkommens-Ansicht begonnen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { WelcomePageComponent } from './components/welcome-page/welcome-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, WelcomePageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Portfolio';
+
+  ngOnInit(): void {
+  }
 }
