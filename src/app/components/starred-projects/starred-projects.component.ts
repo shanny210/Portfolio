d1c610dba6ab2a2bd694d6e485af5260726c9779
Shannon <37170995+shanny210@users.noreply.github.com>
2024-06-04 10:24:37 +0200

    Flip-Animation implementiert

--- src/app/components/starred-projects/starred-projects.component.ts
+++ src/app/components/starred-projects/starred-projects.component.ts
@@ -8,25 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './starred-projects.component.scss'
 })
 export class StarredProjectsComponent {
-  todoCardFlipped = false;
-  musicApiCardFlipped = false;
-  flickPickerFlipped = false;
-  dicerOrDieFlipped = false;
 
-  onFlipCard(projectName: string) {
-  switch(projectName) {
-    case 'todolist':
-      this.todoCardFlipped = !this.todoCardFlipped;
-      break;
-    case 'musicapi':
-      this.musicApiCardFlipped = !this.musicApiCardFlipped;
-      break;
-    case 'flickpicker':
-      this.flickPickerFlipped = !this.flickPickerFlipped;
-      break;
-    case 'diceordie':
-      this.dicerOrDieFlipped = !this.dicerOrDieFlipped;
-      break;
-  }
-  }
 }
